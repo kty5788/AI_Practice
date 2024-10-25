718f09ef6b0b98b67f4cc051c62fd1ea7dc80e29
kangtaeyoon <kty5788kty@gmail.com>
2024-10-25 20:43:43 +0900

    Initial Commit

---
+++ AI_CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  AI_CPP
+//
+//  Created by 강태윤 on 10/25/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
